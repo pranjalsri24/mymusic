7a153b8c28444fc041261f2af3758881f1e7e6bc
pranjal Srivastava <pranjalsrivastava2417@gmail.com>
2023-05-28 17:40:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useState, useEffect} from 'react';
+import Player from './components/Player/Player';
+import song1 from "./music/Har Har Shambhu Shiv Mahadeva_64(PagalWorld.com.se).mp3";
+import song2 from "./music/somebody-new.mp3";
+import song3 from "./music/public_music_on-n-on.mp3";
+import song4 from "./music/Maan Meri Jaan(PagalWorld.com.se).mp3";
 function App() {
+  const [songs] = useState([
+    {
+      title: "song 1",
+      artist: "Machine Gun Kelly",
+      img_src: "./images/song-1.jpg",
+      src: song1
+    },
+    {
+      title: "Song 2",
+      artist:"har har sambhu",
+      img_src: "./images/song-2.jpg",
+      src:  song2
+    },
+    {
+      title: "Song 3",
+      artist: "music on",
+      img_src: "./images/song-3.jpg",
+      src:  song3
+    },
+    {
+      title: "Song 4",
+      artist: "maan meri jaan",
+      img_src: "./images/song-4.jpg",
+      src:  song4
+    }
+  ]);
+
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [nextSongIndex, setNextSongIndex] = useState(0);
+
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      } else {
+        return currentSongIndex + 1;
+      }
+    });
+  }, [currentSongIndex]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Player 
+        currentSongIndex={currentSongIndex} 
+        setCurrentSongIndex={setCurrentSongIndex} 
+        nextSongIndex={nextSongIndex} 
+        songs={songs}
+      />
     </div>
   );
 }
